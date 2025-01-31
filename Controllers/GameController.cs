27e7f836a919c7e38421fd5a7ee54b76c4669c7b
Andrei Daian <andreidaian05@gmail.com>
2025-01-31 02:17:28 +0200

    Added basic controller logic + helper class to convert from model colour to graphical object

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -1,12 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
+using TetrisGame.Views;
+using TetrisGame.Processors.Implementations;
+using Timer = System.Windows.Forms.Timer;
 
 namespace TetrisGame.Controllers
 {
-    internal class GameController
+    public class GameController
     {
+        private readonly Processors.Implementations.TetrisGame _game;
+        private readonly GameView _gameView;
+        private LinePiece _currentPiece;
+        private Timer? _gameTimer;
+
+        public GameController(GameView gameView)
+        {
+            _game = new Processors.Implementations.TetrisGame(20);
+            _gameView = gameView;
+            _currentPiece = new LinePiece();
+            _gameView.KeyDown += OnKeyDown;
+            _gameView.RenderGame(_game.GetLines());
+            StartGameLoop();
+        }
+
+        private void StartGameLoop()
+        {
+            _gameTimer = new Timer { Interval = 500 };
+            _gameTimer.Tick += (sender, args) => MoveDown();
+
+            _gameTimer.Start();
+        }
+
+        private void OnKeyDown(object? sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    _currentPiece.MoveLeft();
+                    break;
+                case Keys.Right:
+                    _currentPiece.MoveRight();
+                    break;
+                case Keys.Down:
+                    MoveDown();
+                    break;
+                default:
+                    _currentPiece.SetSquares();
+                    break;
+            }
+            _gameView.RenderGame(_game.GetLines());
+        }
+
+        private void MoveDown()
+        {
+            _currentPiece.MoveDown();
+            _gameView.RenderGame(_game.GetLines());
+        }
     }
 }
---
+++ Helpers/ColourMapper.cs
@@ -0,0 +1,20 @@
+﻿using TetrisGame.Processors;
+
+namespace TetrisGame.Helpers
+{
+    public static class ColorMapper
+    {
+        public static Color ToColor(Colour colour)
+        {
+            return colour switch
+            {
+                Colour.Red => Color.Red,
+                Colour.Green => Color.Green,
+                Colour.Blue => Color.Blue,
+                Colour.Yellow => Color.Yellow,
+                Colour.Empty => Color.Transparent,
+                _ => throw new ArgumentOutOfRangeException(nameof(colour), colour, "Unexpected colour value.")
+            };
+        }
+    }
+}
