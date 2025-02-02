c3d75dd02427ceb4cb42d5e8cb14ca5129cee682
Andrei Daian <andreidaian05@gmail.com>
2025-02-03 01:56:04 +0200

    Fixed T-Piece rotation

--- Processors/Implementations/Pieces/TPiece.cs
+++ Processors/Implementations/Pieces/TPiece.cs
@@ -19,30 +19,30 @@ namespace TetrisGame.Processors.Implementations
             {
                 case 0: // Default spawn (T-shape facing up)
                     Squares.Add(new Square(new Position(Position.X, Position.Y + 1)));      
-                    Squares.Add(new Square(new Position(Position.X - 1, Position.Y))); 
-                    Squares.Add(new Square(new Position(Position.X + 1, Position.Y)));  
-                    Squares.Add(new Square(new Position(Position.X, Position.Y + 1)));
+                    Squares.Add(new Square(new Position(Position.X - 1, Position.Y + 1))); 
+                    Squares.Add(new Square(new Position(Position.X, Position.Y)));  
+                    Squares.Add(new Square(new Position(Position.X + 1, Position.Y + 1)));
                     break;
 
                 case 1: // 90° (Clockwise)
-                    Squares.Add(new Square(new Position(Position.X, Position.Y)));      
-                    Squares.Add(new Square(new Position(Position.X, Position.Y - 1)));  
                     Squares.Add(new Square(new Position(Position.X, Position.Y + 1)));
-                    Squares.Add(new Square(new Position(Position.X + 1, Position.Y)));
+                    Squares.Add(new Square(new Position(Position.X, Position.Y)));
+                    Squares.Add(new Square(new Position(Position.X + 1, Position.Y + 1)));
+                    Squares.Add(new Square(new Position(Position.X, Position.Y + 2)));
                     break;
 
                 case 2: // 180° (Upside-down)
-                    Squares.Add(new Square(new Position(Position.X, Position.Y)));      
-                    Squares.Add(new Square(new Position(Position.X - 1, Position.Y)));  
-                    Squares.Add(new Square(new Position(Position.X + 1, Position.Y)));  
-                    Squares.Add(new Square(new Position(Position.X, Position.Y - 1)));
+                    Squares.Add(new Square(new Position(Position.X, Position.Y + 1)));      
+                    Squares.Add(new Square(new Position(Position.X + 1, Position.Y + 1)));  
+                    Squares.Add(new Square(new Position(Position.X, Position.Y + 2)));  
+                    Squares.Add(new Square(new Position(Position.X - 1, Position.Y + 1)));
                     break;
 
                 case 3: // 270° (Counterclockwise)
-                    Squares.Add(new Square(new Position(Position.X, Position.Y)));      
-                    Squares.Add(new Square(new Position(Position.X, Position.Y - 1)));
-                    Squares.Add(new Square(new Position(Position.X, Position.Y + 1)));
-                    Squares.Add(new Square(new Position(Position.X - 1, Position.Y)));
+                    Squares.Add(new Square(new Position(Position.X, Position.Y + 1)));      
+                    Squares.Add(new Square(new Position(Position.X, Position.Y + 2)));
+                    Squares.Add(new Square(new Position(Position.X - 1, Position.Y + 1)));
+                    Squares.Add(new Square(new Position(Position.X, Position.Y)));
                     break;
             }
         }
