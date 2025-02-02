f1634bd10612e82a32de107889f45a4815401cff
Andrei Daian <andreidaian05@gmail.com>
2025-02-02 23:41:38 +0200

    Fixed wall kicks for the T-piece

--- Processors/Implementations/Pieces/TPiece.cs
+++ Processors/Implementations/Pieces/TPiece.cs
@@ -16,31 +16,31 @@
             switch (RotationState)
             {
                 case 0: // Default spawn (T-shape facing up)
-                    Squares.Add(new Square(new Position(Position.X, Position.Y)));      // Center
-                    Squares.Add(new Square(new Position(Position.X - 1, Position.Y)));  // Left
-                    Squares.Add(new Square(new Position(Position.X + 1, Position.Y)));  // Right
-                    Squares.Add(new Square(new Position(Position.X, Position.Y + 1)));  // Bottom
+                    Squares.Add(new Square(new Position(Position.X, Position.Y)));      
+                    Squares.Add(new Square(new Position(Position.X - 1, Position.Y))); 
+                    Squares.Add(new Square(new Position(Position.X + 1, Position.Y)));  
+                    Squares.Add(new Square(new Position(Position.X, Position.Y + 1)));
                     break;
 
                 case 1: // 90° (Clockwise)
-                    Squares.Add(new Square(new Position(Position.X, Position.Y)));      // Center
-                    Squares.Add(new Square(new Position(Position.X, Position.Y - 1)));  // Top
-                    Squares.Add(new Square(new Position(Position.X, Position.Y + 1)));  // Bottom
-                    Squares.Add(new Square(new Position(Position.X + 1, Position.Y)));  // Right
+                    Squares.Add(new Square(new Position(Position.X, Position.Y)));      
+                    Squares.Add(new Square(new Position(Position.X, Position.Y - 1)));  
+                    Squares.Add(new Square(new Position(Position.X, Position.Y + 1)));
+                    Squares.Add(new Square(new Position(Position.X + 1, Position.Y)));
                     break;
 
                 case 2: // 180° (Upside-down)
-                    Squares.Add(new Square(new Position(Position.X, Position.Y)));      // Center
-                    Squares.Add(new Square(new Position(Position.X - 1, Position.Y)));  // Left
-                    Squares.Add(new Square(new Position(Position.X + 1, Position.Y)));  // Right
-                    Squares.Add(new Square(new Position(Position.X, Position.Y - 1)));  // Top
+                    Squares.Add(new Square(new Position(Position.X, Position.Y)));      
+                    Squares.Add(new Square(new Position(Position.X - 1, Position.Y)));  
+                    Squares.Add(new Square(new Position(Position.X + 1, Position.Y)));  
+                    Squares.Add(new Square(new Position(Position.X, Position.Y - 1)));
                     break;
 
                 case 3: // 270° (Counterclockwise)
-                    Squares.Add(new Square(new Position(Position.X, Position.Y)));      // Center
-                    Squares.Add(new Square(new Position(Position.X, Position.Y - 1)));  // Top
-                    Squares.Add(new Square(new Position(Position.X, Position.Y + 1)));  // Bottom
-                    Squares.Add(new Square(new Position(Position.X - 1, Position.Y)));  // Left
+                    Squares.Add(new Square(new Position(Position.X, Position.Y)));      
+                    Squares.Add(new Square(new Position(Position.X, Position.Y - 1)));
+                    Squares.Add(new Square(new Position(Position.X, Position.Y + 1)));
+                    Squares.Add(new Square(new Position(Position.X - 1, Position.Y)));
                     break;
             }
         }
@@ -50,15 +50,36 @@
             var oldRotation = RotationState;
             RotationState = (RotationState + 1) % 4;
             DefineShape();
-
-            if (!ValidateRotation())
+            
+            // If rotation is invalid, try wall kicks
+            if (!ValidateRotation()) 
             {
+                // Try shifting left
+                Position = new Position(Position.X - 1, Position.Y);
+                DefineShape();
+                if (ValidateRotation())
+                {
+                    UpdateSquares();
+                    return;
+                }
+                // Try shifting right
+                Position = new Position(Position.X + 2, Position.Y);
+                DefineShape();
+                if (ValidateRotation())
+                {
+                    UpdateSquares();
+                    return;
+                }
+
+                // Undo rotation if all fails
+                Position = new Position(Position.X - 1, Position.Y); // Reset position
                 RotationState = oldRotation;
                 DefineShape();
             }
 
             UpdateSquares();
         }
+
 
         private bool ValidateRotation()
         {
@@ -69,7 +90,7 @@
 
         public override void UpdateSquares()
         {
-            DefineShape(); // Ensures the squares are placed correctly
+            DefineShape();
             ColourSquares();
         }
     }
